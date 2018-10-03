c4a565fc7e7d1f0fca7b787e65866f0b6aa78389
Chris Johnson <csjohnson91@gmail.com>
2018-10-03 17:39:27 +1000

    Moderate refactor of mailer module. Logged an error message and stopped execution if MongoDB not available. Reduced a little nesting, fixed some typos.

--- mailer/mailer.js
+++ mailer/mailer.js
@@ -4,44 +4,45 @@ const emailSender = require("../email-sender/email-sender");
 const mins = 10;
 
 const ozbmailer = database => {
-  // The deal scrapper
+  if (!database) {
+    console.error("MongoDB database is not available.");
+    return;
+  }
+  // The deal scraper
   dealScraper(goodDeals => {
-    // If there are any good deal returned from the scraper
-    if (goodDeals.length > 0) {
-      // Map through them
-      goodDeals.map(deal => {
-        // Insert it to the database first
-        database.collection("deals").insertOne(deal, err => {
-          // Handle database returning errors
-          if (err) {
-            // The most likely one is the duplicated error
-            if (err.name === "MongoError" && err.code === 11000) {
-              console.log("No new good deal! Just old deal duplicated!");
-            } else {
-              console.log(err);
-            }
-          } else {
-            // Print out the deal title to the console
-            console.log("New deal found: " + deal.title);
-            // Get all emails from database, send the deal to each of them
-            database
-              .collection("emails")
-              .find()
-              .each((err, email) => {
-                // .each will return null at the end so make sure you check for email availability
-                if (email) {
-                  const message = emailMessage(email.email_address, deal);
-                  emailSender(message, email);
-                }
-              });
-          }
-        });
-      });
-    } else {
-      console.log("No good deal!");
+    if (!goodDeals || goodDeals.length < 1) {
+      console.log("No good deals!");
+      return;
     }
+    // If there are any good deal returned from the scraper map them
+    goodDeals.map(deal => {
+      // Insert it to the database first
+      database.collection("deals").insertOne(deal, err => {
+        // Handle database errors
+        if (err) {
+          // The most likely one is the duplicated error
+          if (err.name === "MongoError" && err.code === 11000) {
+            console.log("No new good deal! Just old deal duplicated!");
+          } else {
+            console.error(err);
+          }
+        } else {
+          console.log("New deal found: " + deal.title);
+          // Get all emails from database, send the deal to each of them
+          database
+            .collection("emails")
+            .find()
+            .each((err, email) => {
+              // .each will return null at the end so make sure you check for email availability
+              if (email) {
+                const message = emailMessage(email.email_address, deal);
+                emailSender(message, email);
+              }
+            });
+        }
+      });
+    });
+    console.log("Next fetch in " + mins + " minutes");
   });
-  console.log("Next fetch in " + mins + " minutes");
 };
-
 module.exports = ozbmailer;
